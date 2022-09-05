edf0cb53b14bd62c4c34901310336cb4554c48aa
Lessalc <lucianoclessa@gmail.com>
2022-09-04 23:53:45 -0300

    Saving new Department

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -8,7 +8,7 @@ public class Department implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private Integer id;
-	private String name;
+	private String name = "";
 	
 	public Department() {
 		
